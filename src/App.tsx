031d39080c2e27fa6e889722625a4dbe5b207dba
naveenbabu04 <saiajithnaveenbabu.bahunadham1@ingka.com>
2024-03-20 16:18:28 +0530

    new files added

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const Home = lazy( () => import("./pages/home"));
 const App = () => {
   return <Router>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/portfolio" element={<Home />} />
     </Routes>
   </Router>
 };
